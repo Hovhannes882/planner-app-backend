eea4fddc7f8e029b720e838038ed1f8e79a7e9de
Ghukasyan <hovhannes.ghukasyan@ekeng.am>
2026-09-23 17:01:28 +0400

    Login functionality

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\API\Auth\LoginRequest;
 use App\Http\Requests\API\Auth\SignupRequest;
 use App\Models\User;
+use Hash;
 
 class AuthController extends Controller
 {
@@ -21,6 +23,43 @@ class AuthController extends Controller
 
             return response()->json([
                 "message" => "success"
+            ]);
+        } catch (\Throwable $th) {
+            return response()->json(["message" => $th->getMessage()], 500);
+        }
+    }
+
+    /**
+     * Summary of login
+     * 
+     * @param LoginRequest $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function login(LoginRequest $request)
+    {
+        try {
+            $loginType = "email";
+            if ($request->has("username")) {
+                $loginType = "username";
+            }
+
+            $user = User::where($loginType, $request->input($loginType))->first();
+
+            if (!$user) {
+                return response()->json(["message" => "User not found"], 401);
+            }
+
+            if (!Hash::check($request->input("password"), $user->password)) {
+                return response()->json(["message" => "Invalid credentials"], 401);
+            }
+
+            $token = $user->createToken('api-token')->plainTextToken;
+
+
+            return response()->json([
+                "message" => "success",
+                "data" => $user,
+                "token" => $token,
             ]);
         } catch (\Throwable $th) {
             return response()->json(["message" => $th->getMessage()], 500);
