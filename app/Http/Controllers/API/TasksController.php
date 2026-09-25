38777f133ea8886344eb0b6fcab1f2b93f142b58
Ghukasyan <hovhannes.ghukasyan@ekeng.am>
2026-09-25 15:05:16 +0400

    Migrations tasks, aspects

---
+++ app/Http/Controllers/API/TasksController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    public function create()
+    {
+
+    }
+}
